c147efcb9c896941f3264404b22fedba8c895b9f
Shi Dong <shidong1986@gmail.com>
2018-01-08 22:14:53 -0500

    Update the data preprocessing script

--- data_preprocessing/data_stats.py
+++ data_preprocessing/data_stats.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import collections
 import csv
+import math
 
 def main():
   num_args = len(sys.argv)
@@ -14,31 +15,99 @@ def main():
   feature_list = []
   study_id_list = []
   df_dict = collections.OrderedDict()
+
+  no_ambiguous_data = True
+  # Get the dataframe
   for file_name in arg_list:
     features = []
     df_dict[file_name] = pd.read_csv(file_name)
+    if no_ambiguous_data:
+      df_dict[file_name].replace('888', np.nan, inplace=True, regex=True)
+      df_dict[file_name].replace('999', np.nan, inplace=True, regex=True)
     features = list(df_dict[file_name].columns.values)
+    indices = df_dict[file_name].index.tolist()
     feature_list += features
     study_id_list += list(df_dict[file_name]["STUDY_ID"])
 
+    ## Preliminary missing data handling
+    #pos_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 2].tolist()
+    #neg_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 1].tolist()
+    #for feat in features:
+    #  null_flags = list(df_dict[file_name][feat].isnull())
+    #  if any(null_flags):
+    #    pos_samples = list(df_dict[file_name][feat][pos_index])
+    #    neg_samples = list(df_dict[file_name][feat][neg_index])
+    #    feat_values_counts_pos = collections.Counter(pos_samples)
+    #    major_feat_value_pos = feat_values_counts_pos.most_common(1)[0][0]
+    #    feat_values_counts_neg = collections.Counter(neg_samples)
+    #    major_feat_value_neg = feat_values_counts_neg.most_common(1)[0][0]
+    #    for i in pos_index:
+    #      if np.isnan(df_dict[file_name][feat][i]):
+    #        df_dict[file_name].at[i, feat] = major_feat_value_pos
+    #    for i in neg_index:
+    #      if np.isnan(df_dict[file_name][feat][i]):
+    #        df_dict[file_name].at[i, feat] = major_feat_value_neg
+    #pos_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 2].tolist()
+    #neg_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 1].tolist()
+    #all_index = df_dict[file_name].index.tolist()
+    #for feat in features:
+    #  null_flags = list(df_dict[file_name][feat].isnull())
+    #  if any(null_flags):
+    #    samples = list(df_dict[file_name][feat])
+    #    feat_values_counts = collections.Counter(samples)
+    #    major_feat_value = feat_values_counts.most_common(1)[0][0]
+    #    for i in all_index:
+    #      if np.isnan(df_dict[file_name][feat][i]):
+    #        #df_dict[file_name].at[i, feat] = major_feat_value
+    #        df_dict[file_name].at[i, feat] = 555
+
+    # Missing data handling using similarity
+    num_sample = len(indices)
+    num_feature = len(features)
+
+    # Initialize the similarity matrix
+    similarity_mat = []
+    for i in range(num_sample):
+      similarity_mat.append([-999999]*num_sample)
+
+    # Do the calculation
+    for i in range(num_sample):
+      data_i = list(df_dict[file_name].loc[int(indices[i])])
+      for j in range(i+1, num_sample):
+        similarity = 0
+        count = 0
+        data_j = list(df_dict[file_name].loc[int(indices[j])])
+        for f in range(num_feature):
+          if np.isnan(data_i[f]) or np.isnan(data_j[f]):
+            continue
+          else:
+            similarity -= (float(data_i[f]) - float(data_j[f])) * (float(data_i[f]) - float(data_j[f]))
+            count += 1
+        similarity_mat[i][j] = similarity# / float(count)
+        similarity_mat[j][i] = similarity# / float(count)
+    print "Similarity matrix construction done"
+
     # Missing data handling
-    pos_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 2].tolist()
-    neg_index = df_dict[file_name].index[df_dict[file_name]['PPTERM'] == 1].tolist()
-    for feat in features:
-      null_flags = list(df_dict[file_name][feat].isnull())
-      if any(null_flags):
-        pos_samples = list(df_dict[file_name][feat][pos_index])
-        neg_samples = list(df_dict[file_name][feat][neg_index])
-        feat_values_counts_pos = collections.Counter(pos_samples)
-        major_feat_value_pos = feat_values_counts_pos.most_common(1)[0][0]
-        feat_values_counts_neg = collections.Counter(neg_samples)
-        major_feat_value_neg = feat_values_counts_neg.most_common(1)[0][0]
-        for i in pos_index:
-          if np.isnan(df_dict[file_name][feat][i]):
-            df_dict[file_name].at[i, feat] = major_feat_value_pos
-        for i in neg_index:
-          if np.isnan(df_dict[file_name][feat][i]):
-            df_dict[file_name].at[i, feat] = major_feat_value_neg
+    num_try = 100
+    for i in range(num_sample):
+      data_i = list(df_dict[file_name].loc[int(indices[i])])
+      for f in range(num_feature):
+        if np.isnan(data_i[f]):
+          similarity_sample = similarity_mat[i]
+          sorted_similarity_sample = sorted(similarity_sample, reverse=True)
+          sorted_index = [index[0] for index in sorted(enumerate(similarity_sample), key=lambda x:x[1], reverse=True)]
+          for num in range(num_try):
+            data_x = list(df_dict[file_name].loc[int(indices[sorted_index[num]])])
+            if np.isnan(data_x[f]):
+              continue
+            else:
+              df_dict[file_name].at[features[f], i] = data_x[f]
+              print "Feature:", f
+              print "Selected Similarity:", sorted_similarity_sample[sorted_index[num]]
+              break
+          if np.isnan(data_i[f]):
+            print "Number of try is not enough"
+            exit()
 
   # Compute the statistics of features and study_id
   num_file = len(arg_list)
@@ -66,7 +135,7 @@ def main():
   # Re-generate combined data
   keys = df_dict.keys()
   header = []
-  fd = open("combined.csv", "wb")
+  fd = open("combined_v1_v2.csv", "wb")
   file_writer = csv.writer(fd)
   for key in keys:
     for feature in overlapped_features:
@@ -82,21 +151,37 @@ def main():
         remove_idx += df_dict[key].index[df_dict[key]['STUDY_ID'] == study_id].tolist()
     for index in sorted(remove_idx, reverse=True):
       df_dict[key] = df_dict[key].drop(df_dict[key].index[index])
-    #df_dict[key].to_csv("unique_"+key)
-    #continue
+    overlapped_data = False
+    if overlapped_data == True:
+      df_dict[key].to_csv("overlapped_"+key)
+      continue
 
     if key == keys[-1]:
       temp = list(df_dict[key].columns.values)
       header += temp[1:]
     elif key == keys[0]:
       df_dict[key].drop("PPTERM", axis=1, inplace=True)
-      header += list(df_dict[key].columns.values)
+      temp = list(df_dict[key].columns.values)
+      header += temp[1:]
     else:
       df_dict[key].drop("PPTERM", axis=1, inplace=True)
       temp = list(df_dict[key].columns.values)
       header += temp[1:]
+
   file_writer.writerow(header)
 
+  #numerical_field_indicator = open("numerical_chosenfields.csv", "rb")
+  #file_reader = csv.reader(numerical_field_indicator)
+  #numerical_fields_list = []
+  #numerical_fields_index_list = []
+  #for field in file_reader:
+  #  if field[0] in header:
+  #    numerical_fields_list.append(field[0])
+  #    numerical_fields_index_list.append(header.index(field[0]))
+  #print len(numerical_fields_list), "features for calculating similarity matrix"
+
+  # Get raw data
+  data = []
   for study_id in overlapped_study_id:
     row = []
     for key in df_dict:
@@ -104,13 +189,68 @@ def main():
       temp = list(df_dict[key].loc[int(idx[0])])
       for i in range(len(temp)):
         if np.isnan(temp[i]):
-          temp[i] = ""
+          print "Something is wrong"
+          exit()
       if key == keys[-1]:
         row += temp[1:]
       elif key == keys[0]:
-        row += temp
+        row += temp[1:]
       else:
         row += temp[1:]
+    data.append(row)
+
+  ##
+  # Calculate similarity matrix
+  ##
+  #sub_data = []
+  #for sample in data:
+  #  sub_data.append([sample[i] for i in numerical_fields_index_list])
+
+  #num_sample = len(data)
+  #num_feature = len(data[0])
+  #num_sub_feature = len(sub_data[0])
+
+  ## Initialize the similarity matrix
+  #similarity_mat = []
+  #for i in range(num_sample):
+  #  similarity_mat.append([-999999]*num_sample)
+
+  ## Do the calculation
+  #for i in range(num_sample):
+  #  for j in range(i+1, num_sample):
+  #    similarity = 0
+  #    count = 0
+  #    for f in range(num_sub_feature):
+  #      if data[i][f] == "" or data[j][f] == "":
+  #        continue
+  #      else:
+  #        similarity -= float(data[i][f] - data[j][f]) * float(data[i][f] - data[j][f])
+  #        count += 1
+  #    similarity_mat[i][j] = similarity / float(count)
+  #    similarity_mat[j][i] = similarity / float(count)
+  #print "Similarity matrix construction done"
+
+  ## Missing data handling
+  #num_try = 100
+  #for i in range(num_sample):
+  #  for f in range(num_feature):
+  #    if data[i][f] == "":
+  #      similarity_sample = similarity_mat[i]
+  #      sorted_similarity_sample = sorted(similarity_sample, reverse=True)
+  #      sorted_index = [index[0] for index in sorted(enumerate(similarity_sample), key=lambda x:x[1], reverse=True)]
+  #      for num in range(num_try):
+  #        if data[sorted_index[num]][f] == "":
+  #          continue
+  #        else:
+  #          data[i][f] = data[sorted_index[num]][f]
+  #          print "Feature:", header[f]
+  #          print "Selected Similarity:", sorted_similarity_sample[sorted_index[num]]
+  #          break
+  #      if data[i][f] == "":
+  #        print "Number of try is not enough"
+  #        exit()
+
+  for row in data:
     file_writer.writerow(row)
 
 if __name__ == "__main__":
